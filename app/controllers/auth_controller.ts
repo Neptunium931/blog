e6f28101147300eb3abc48604d9bf650b7ed8c82
neptumiun <tymothe.billerey@gmail.com>
2024-05-23 15:32:57 +0200

    feat(auth): login and register

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,38 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+
+export default class AuthController {
+  public showLoginForm({ view }: HttpContext) {
+    return view.render('auth/login')
+  }
+
+  public async login({ request, auth, response }: HttpContext) {
+    const { email, password } = request.only(['email', 'password'])
+
+    const user = await User.verifyCredentials(email, password)
+
+    await auth.use('web').login(user)
+
+    return response.redirect().back()
+  }
+
+  public async logout({ auth, response }: HttpContext) {
+    await auth.use('web').logout()
+
+    return response.redirect().back()
+  }
+
+  public showRegisterForm({ view }: HttpContext) {
+    return view.render('auth/register')
+  }
+
+  public async register({ request, auth, response }: HttpContext) {
+    const data = request.only(['username', 'email', 'password'])
+
+    const user = await User.create(data)
+
+    await auth.use('web').login(user)
+
+    return response.redirect().toRoute('home')
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,21 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+import AuthController from '#controllers/auth_controller'
+import UsersController from '#controllers/users_controller'
+import { middleware } from '#start/kernel'
+
+router.on('/').render('pages/home').as('home')
+
+router.get('users', [UsersController, 'list']).as('users')
+
+router.group(() => {
+  router.get('login', [AuthController, 'showLoginForm'])
+  router.post('login', [AuthController, 'login']).as('login')
+  router.get('register', [AuthController, 'showRegisterForm'])
+  router.post('register', [AuthController, 'register']).as('register')
+}).middleware(middleware.guest())
+
+router.group(() => {
+  router.get('logout', [AuthController, 'logout']).as('logout')
+}).middleware(middleware.auth())
