66436dedc4511aab2947928ecbaa50683a433224
neptumiun <tymothe.billerey@gmail.com>
2024-05-23 10:00:06 +0200

    add sqllite for dev

--- config/database.ts
+++ config/database.ts
@@ -1,24 +1,46 @@
-import env from '#start/env'
+import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
+import env from '#start/env'
 
-const dbConfig = defineConfig({
-  connection: 'postgres',
-  connections: {
-    postgres: {
-      client: 'pg',
-      connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
-      },
-      migrations: {
-        naturalSort: true,
-        paths: ['database/migrations'],
+let dbConfig
+
+if (env.get('DB_CONNECTION') === 'sqlite') {
+  dbConfig = defineConfig({
+    connection: 'sqlite',
+    connections: {
+      sqlite: {
+        client: 'better-sqlite3',
+        connection: {
+          filename: app.tmpPath('db.sqlite3')
+        },
+        useNullAsDefault: true,
+        migrations: {
+          naturalSort: true,
+          paths: ['database/migrations'],
+        },
       },
     },
-  },
-})
+  })
+} else {
+  dbConfig = defineConfig({
+    connection: 'postgres',
+    connections: {
+      postgres: {
+        client: 'pg',
+        connection: {
+          host: env.get('DB_HOST'),
+          port: env.get('DB_PORT'),
+          user: env.get('DB_USER'),
+          password: env.get('DB_PASSWORD'),
+          database: env.get('DB_DATABASE'),
+        },
+        migrations: {
+          naturalSort: true,
+          paths: ['database/migrations'],
+        },
+      },
+    },
+  })
+}
 
 export default dbConfig
